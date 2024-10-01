9fffabed1bed687dce633dec49509adbfd21ff12
LARCHIK <ovchinnikov20035696@gmail.com>
2024-10-01 15:04:27 +0500

    Initial commit

---
+++ src/test/java/ru/nikita/naujava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikita.naujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
