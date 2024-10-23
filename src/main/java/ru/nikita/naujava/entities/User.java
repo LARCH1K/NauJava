b9dd8dea4fdc3115a8748a507162d6cd69528e4f
LARCHIK <ovchinnikov20035696@gmail.com>
2024-10-23 21:27:15 +0500

    Done homework - Spring Security

---
+++ src/main/java/ru/nikita/naujava/entities/User.java
@@ -0,0 +1,35 @@
+package ru.nikita.naujava.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+}
